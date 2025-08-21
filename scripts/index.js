93335cf8ffe0a089b30220eade9c3793108f4454
Zero // Alexis <128416166+ZPAlexis@users.noreply.github.com>
2025-08-21 15:48:47 -0300

    Transition intro on Index to DOM

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.querySelector('.js-intro-title')
+  .innerHTML = 'Hey! This is an Intro Title.';
+
+document.querySelector('.js-intro-description')
+  .innerHTML = 'And here is the intro description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+document.querySelector('.js-intro-cta')
+  .innerHTML = 'Learn more <a class="link" href="about.html">About Us &gt;</a>';
