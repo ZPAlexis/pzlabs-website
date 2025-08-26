624df03b07d9072bfbab72926c02abd1836a61de
Zero // Alexis <128416166+ZPAlexis@users.noreply.github.com>
2025-08-25 21:44:44 -0300

    AutoText type and delete

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,11 @@ img.addEventListener('click', () => {
   img.classList.add('spin');
 });
 
-let autoTextHTML = '';
+const autoTextHTML = document.querySelector('.auto-text');
 const verticalBarBlinkHTML = document.querySelector('.vertical-bar-blink');
-const autoTextOptions = [
-  'Text 1',
-  'Text 2',
-  'Text 3'
-];
 
-/*autoTextOptions.forEach((textOption) => {
-  autoTextHTML += textOption + ' ';
-});
-
-//document.querySelector('.intro-auto-text')
-  .innerHTML = autoTextHTML;*/
+const autoText = "Creating";
+let i = 0;
 
 //add letter up to the last letter of the word, each at a time, with a quick delay between them
 //blink dash | at the end for a few seconds
@@ -26,21 +17,40 @@ const autoTextOptions = [
 //repeat for each word
 //then loop through list again
 
-async function autoText() {
+async function typeAutoText() {
 //vertical bar blink
 //blink dash out and in once, control delay
 //start first letter of the first word
-};
+  if (i < autoText.length) {
+    autoTextHTML.innerHTML += autoText[i];
+    i++;
+    setTimeout(typeAutoText, 80);
+  } else {
+    i = 0;
+    setTimeout(eraseAutoText, 3000);
+  }
+}
+
+async function eraseAutoText() {
+  if (autoTextHTML.innerHTML.length != 0) {
+    autoTextHTML.innerHTML = autoTextHTML.innerHTML.slice(0, -1);
+    setTimeout(eraseAutoText, 80);
+  } else {
+    setTimeout(typeAutoText, 300);
+  }
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function verticalBarBlink() {
-  await sleep(600);
+  await sleep(1000);
   verticalBarBlinkHTML.innerHTML = '&nbsp;';
-  await sleep(300);
+  await sleep(400);
   verticalBarBlinkHTML.innerHTML = '|';
-};
+  setTimeout(verticalBarBlink, 80);
+}
 
 verticalBarBlink();
+typeAutoText();
