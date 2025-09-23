831c46f1d096d2a2f1d67259732c3c20d8f313b4
Zero // Alexis <128416166+ZPAlexis@users.noreply.github.com>
2025-09-23 12:32:31 -0300

    Cover box switch to coin with initial animations for testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,18 @@
-const img = document.querySelector('.intro-image');
-img.addEventListener('click', () => {
-  img.classList.remove('spin');
-  void img.offsetWidth;
-  img.classList.add('spin');
+const button = document.querySelector('.cover-btn');
+const boxImg = button.querySelector('.js-cover-box');
+const coinImg = button.querySelector('.js-cover-coin');
+
+let coverCoinCollected = false;
+
+button.addEventListener('click', () => {
+  if (!coverCoinCollected) {
+    boxImg.classList.remove('idle');
+    boxImg.classList.add('open');
+    coinImg.classList.add('visible');
+    coverCoinCollected = true;
+  } else {
+    coinImg.classList.remove('spin');
+    void coinImg.offsetWidth;
+    coinImg.classList.add('spin');
+  }
 });
-/*
-const autoTextHTML = document.querySelector('.auto-text');
-const verticalBarBlinkHTML = document.querySelector('.vertical-bar-blink');
-
-const autoTextOptions = [
-  'Engineering',
-  'Designing',
-  'Coding',
-  'Developing',
-  'Researching',
-  'Scoping',
-  'Reviewing',
-  'Creating',
-  'Enhancing',
-  'Ideating',
-  'Implementing',
-  'Workshopping'
-]
-let autoText = '';
-let i1 = 0;
-let i2 = 0;
-
-//startAutoText
-//chooseAutoText < loop start
-//short delay
-//stop vertical blinking
-//typeAutoText
-//resume vertical blinking
-//short delay
-//stop vertical blinking
-//eraseAutoText
-//resume vertical blinking
-//chooseAutoText < loop start
-
-function chooseAutoText() {
-  if (i1 < autoTextOptions.length) {
-    autoText = autoTextOptions[i1];
-    i1++;
-  } else {
-    i1 = 0;
-    autoText = autoTextOptions[i1];
-    i1++;
-  }
-}
-
-async function typeAutoText() {
-  if (i2 < autoText.length) {
-    autoTextHTML.innerHTML += autoText[i2];
-    i2++;
-    setTimeout(typeAutoText, 80);
-  } else {
-    i2 = 0;
-    chooseAutoText();
-    setTimeout(eraseAutoText, 2000);
-  }
-}
-
-async function eraseAutoText() {
-  if (autoTextHTML.innerHTML.length != 0) {
-    autoTextHTML.innerHTML = autoTextHTML.innerHTML.slice(0, -1);
-    setTimeout(eraseAutoText, 60);
-  } else {
-    setTimeout(typeAutoText, 300);
-  }
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function verticalBarBlink() {
-  await sleep(1000);
-  verticalBarBlinkHTML.innerHTML = '&nbsp;';
-  await sleep(400);
-  verticalBarBlinkHTML.innerHTML = '|';
-  setTimeout(verticalBarBlink, 80);
-}
-
-verticalBarBlink();
-chooseAutoText();
-typeAutoText();
-*/
