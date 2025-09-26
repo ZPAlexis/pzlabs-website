034d1d320b7c45550c5f77679fe6cd46ee1cdb29
Zero // Alexis <128416166+ZPAlexis@users.noreply.github.com>
2025-09-25 23:26:43 -0300

    Button with handlePressStart and End instead of click and border to outline changes on summary container

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -2,6 +2,7 @@ import { fillBar } from './fillBar.js';
 
 const fillBarButton = document.querySelector('.js-fill-bar-play-button');
 
+/*
 fillBarButton.onclick = function () {
   fillBar(6);
   fillBarButton.classList.add('pressed');
@@ -9,3 +10,24 @@ fillBarButton.onclick = function () {
     fillBarButton.classList.remove('pressed');
   }, 100);
 };
+*/
+
+// Apply .pressed immediately on touchstart or mousedown
+function handlePressStart() {
+  fillBar(6);
+  fillBarButton.classList.add('pressed');
+}
+
+// Remove .pressed after interaction ends
+function handlePressEnd() {
+  fillBarButton.classList.remove('pressed');
+}
+
+// Touch (mobile)
+fillBarButton.addEventListener('touchstart', handlePressStart);
+fillBarButton.addEventListener('touchend', handlePressEnd);
+fillBarButton.addEventListener('touchcancel', handlePressEnd); // just in case
+
+// Mouse (desktop)
+fillBarButton.addEventListener('mousedown', handlePressStart);
+fillBarButton.addEventListener('mouseup', handlePressEnd);
